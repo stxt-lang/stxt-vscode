51f78acd67fbc91fd9b1bbbdcff8a764ff6218c2
Joan Costa Mombiela <joan.costa.mombiela@gmail.com>
2026-02-08 20:28:27 +0100

    M+

---
+++ stxt/out/all.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let salutacio = "Hola, món!";
+console.log(salutacio);
+//# sourceMappingURL=all.js.map
---
+++ stxt/src/all.ts
@@ -0,0 +1,2 @@
+let salutacio: string = "Hola, món!";
+console.log(salutacio);
