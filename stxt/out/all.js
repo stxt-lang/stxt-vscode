c34d00c64eb8557d8d1d02b8bb50e7d65029e83b
Joan Costa Mombiela <joan.costa.mombiela@gmail.com>
2026-02-09 19:53:53 +0100

    M+

--- stxt/out/all.js
+++ stxt/out/all.js
@@ -2,10 +2,22 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const promises_1 = require("fs/promises");
 const parserSTXT_1 = require("./parserSTXT");
+const Parser_1 = require("./Parser");
 async function main() {
     const texto = await (0, promises_1.readFile)("demo.stxt", "utf-8");
     //console.log(texto);
     (0, parserSTXT_1.parseSTXT)(texto);
+    const parser = new Parser_1.Parser();
+    const nodes = parser.parse(texto);
+    for (let i = 0; i < nodes.length; i++) {
+        const node = nodes[i];
+        console.log(`Node ${i + 1}: ${node}`);
+        const children = node.getChildren();
+        for (let c = 0; c < children.length; c++) {
+            const child = children[c];
+            console.log(`\tChild ${i + 1}: ${child}`);
+        }
+    }
 }
 main();
 console.log("Hola mundo!!");
--- stxt/src/all.ts
+++ stxt/src/all.ts
@@ -1,12 +1,30 @@
 import { readFile } from "fs/promises";
 import { text } from "stream/consumers";
 import { parseSTXT } from "./parserSTXT";
+import { Parser } from "./Parser";
+import { Node } from "./Node";
 
 async function main() {
     const texto: string = await readFile("demo.stxt", "utf-8");
     //console.log(texto);
 
     parseSTXT(texto);
+
+    const parser:Parser = new Parser();
+    const nodes: Node[] = parser.parse(texto);
+
+    for(let i = 0; i<nodes.length; i++)
+    {
+        const node = nodes[i];
+        console.log(`Node ${i+1}: ${node}`);
+
+        const children = node.getChildren();
+        for (let c = 0; c<children.length; c++)
+        {
+            const child = children[c];
+            console.log(`\tChild ${i+1}: ${child}`);
+        }
+    }
 }
 
 main();
