33b92ea5e3ff5a7440525df7afa911ed6cd65e34
Joan Costa <jcostamo@fiatc.es>
2026-02-11 12:05:17 +0100

    M+

--- stxt/out/StxtSemanticTokensProvider.js
+++ stxt/out/StxtSemanticTokensProvider.js
@@ -35,11 +35,7 @@ var __importStar = (this && this.__importStar) || (function () {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.StxtSemanticTokensProvider = exports.tokenLegend = void 0;
 const vscode = __importStar(require("vscode"));
-const Node_1 = require("./core/Node");
-const LineIndentParser_1 = require("./core/LineIndentParser");
-// *****************
-// Tokens semánticos
-// *****************
+const STXTAnalysis_1 = require("./STXTAnalysis");
 const tokenTypes = [
     'keyword',
     'property',
@@ -50,28 +46,16 @@ const tokenTypes = [
 exports.tokenLegend = new vscode.SemanticTokensLegend(tokenTypes);
 class StxtSemanticTokensProvider {
     provideDocumentSemanticTokens(document) {
-        console.log("Semantics init...");
         const builder = new vscode.SemanticTokensBuilder(exports.tokenLegend);
-        const lines = document.getText().split(/\r?\n/);
-        const stack = [];
-        let lastNode = new Node_1.Node(0, 0, "Init", null, false, "");
-        lines.forEach((line, lineIndex) => {
-            const lastLevel = lastNode ? lastNode.getLevel() : 0;
-            const lastNodeText = lastNode ? lastNode.isTextNode() : false;
-            // Parseamos línea
-            try {
-                const lineIndent = LineIndentParser_1.LineIndentParser.parseLine(line, lastNodeText, lastLevel, lineIndex + 1);
+        const analysis = (0, STXTAnalysis_1.getLastAnalysis)(document);
+        if (!analysis) {
+            return builder.build();
+        }
+        for (const t of analysis.tokens) {
+            if (t.type === 'comment') {
+                builder.push(t.line, t.startChar, t.length, tokenTypes.indexOf('comment'));
             }
-            catch (e) {
-                //console.log("Error: " + e);
-                // Añadir a errores!! ¿Está bien?
-            }
-            // Comment (hacer mejor)
-            if (line.trim().startsWith("#")) {
-                builder.push(lineIndex, 0, line.length, tokenTypes.indexOf('comment'));
-            }
-        });
-        console.log("Semantics end.");
+        }
         return builder.build();
     }
 }
--- stxt/src/StxtSemanticTokensProvider.ts
+++ stxt/src/StxtSemanticTokensProvider.ts
@@ -1,54 +1,36 @@
 import * as vscode from 'vscode';
-import { Node } from './core/Node';
-import { LineIndent } from './core/LineIndent';
-import { LineIndentParser } from './core/LineIndentParser';
-
-// *****************
-// Tokens semánticos
-// *****************
+import { getLastAnalysis } from './STXTAnalysis';
 
 const tokenTypes = [
-    'keyword',
-    'property',
-    'string',
-    'variable',
-    'comment'
+	'keyword',
+	'property',
+	'string',
+	'variable',
+	'comment'
 ];
 
 export const tokenLegend = new vscode.SemanticTokensLegend(tokenTypes);
 
 export class StxtSemanticTokensProvider implements vscode.DocumentSemanticTokensProvider {
+	provideDocumentSemanticTokens(document: vscode.TextDocument): vscode.ProviderResult<vscode.SemanticTokens> {
+		const builder = new vscode.SemanticTokensBuilder(tokenLegend);
 
-    provideDocumentSemanticTokens(document: vscode.TextDocument): vscode.ProviderResult<vscode.SemanticTokens> {
-        console.log("Semantics init...");
-        const builder = new vscode.SemanticTokensBuilder(tokenLegend);
-        const lines = document.getText().split(/\r?\n/);
+		const analysis = getLastAnalysis(document);
+		if (!analysis) {
+			return builder.build();
+		}
 
-        const stack: Node[] = [];
-        let lastNode: Node = new Node(0,0,"Init",null,false,"");
+		for (const t of analysis.tokens) {
+			if (t.type === 'comment') {
+				builder.push(
+					t.line,
+					t.startChar,
+					t.length,
+					tokenTypes.indexOf('comment')
+				);
+			}
+		}
 
-        lines.forEach((line, lineIndex) => {
-            const lastLevel = lastNode ? lastNode.getLevel() : 0;
-            const lastNodeText = lastNode ? lastNode.isTextNode() : false;
-
-            // Parseamos línea
-            try
-            {
-            const lineIndent: LineIndent | null = LineIndentParser.parseLine(line,lastNodeText,lastLevel,lineIndex + 1);
-            }
-            catch(e)
-            {
-                //console.log("Error: " + e);
-                // Añadir a errores!! ¿Está bien?
-            }
-            
-            // Comment (hacer mejor)
-            if (line.trim().startsWith("#")) {
-                builder.push(lineIndex, 0, line.length, tokenTypes.indexOf('comment'));
-            }
-        });
-
-        console.log("Semantics end.");
-        return builder.build();
-    }
+		return builder.build();
+	}
 }
