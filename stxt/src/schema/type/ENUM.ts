f4cf095c1e257def03673d16d48327565e3d8c8c
Joan Costa Mombiela <joan.costa.mombiela@gmail.com>
2026-02-12 18:51:55 +0100

    M+

---
+++ stxt/src/schema/type/ENUM.ts
@@ -0,0 +1,33 @@
+// type/ENUM.ts
+
+import { Node } from "../../core/Node";
+import { ValidationException } from "../../exceptions/ValidationException";
+import { NodeDefinition } from "../NodeDefinition";
+import { Type } from "../Type";
+
+export const ENUM: Type = {
+    getName(): string {
+        return "ENUM";
+    },
+
+    validate(ndef: NodeDefinition, n: Node): void {
+        if (n.getTextLines().length > 0) {
+            throw new ValidationException(
+                n.getLine(),
+                "NOT_ALLOWED_TEXT",
+                `Not allowed text in node ${n.getQualifiedName()}`
+            );
+        }
+
+        const value = n.getValue();
+        const allowed = ndef.getValues(); // ReadonlySet<string>
+
+        if (!allowed.has(value)) {
+            throw new ValidationException(
+                n.getLine(),
+                "INVALID_VALUE",
+                `The value '${value}' not allowed. Only: ${Array.from(allowed).join(", ")}`
+            );
+        }
+    },
+};
