1f25f1174b437a3c741f72edf1ad54b305de0d69
Joan Costa Mombiela <joan.costa.mombiela@gmail.com>
2026-02-14 09:02:19 +0100

    M+

--- stxt/src/extension/AnalysisResult.ts
+++ stxt/src/extension/AnalysisResult.ts
@@ -2,6 +2,6 @@ import { Node } from '../core/Node';
 import type { StxtToken, StxtTokenType } from './Tokens';
 
 export interface AnalysisResult {
-  tokens: StxtToken[];
-  nodeByLine: Map<number, Node>;
+	tokens: StxtToken[];
+	nodeByLine: Map<number, Node>;
 }
